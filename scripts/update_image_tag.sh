4626852a7fe4836b3f4e141a1109cae103b3e0d2
dvdvlrss <davide.valoroso14@gmail.com>
2026-04-22 17:24:06 +0200

    Mark update_image_tag.sh as executable and fix image patch

--- scripts/update_image_tag.sh
+++ scripts/update_image_tag.sh
@@ -2,13 +2,9 @@
 set -euo pipefail
 
 NEW_IMAGE="$1"
-
 FILE="k3s/ot-consumer-deploy.yaml"
 
-# Aggiorna SOLO il Deployment:
-# .spec.template.spec.containers[0].image del primo documento (Deployment)
-# Il Service (secondo documento) resta intatto.
-yq -i '
-  (select(.kind == "Deployment")
-    | .spec.template.spec.containers[0].image) = strenv(NEW_IMAGE)
-' "$FILE"
+yq -i "
+  (select(.kind == \"Deployment\")
+    | .spec.template.spec.containers[0].image) = \"${NEW_IMAGE}\"
+" "$FILE"
